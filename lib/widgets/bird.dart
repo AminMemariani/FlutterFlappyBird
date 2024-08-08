cf57d54b42cac20ab4f167f65a1e6a1233425c03
Amin Memariani <amin90memar@gmail.com>
2024-08-08 12:48:31 +0330

    Added bird widget and assets folder

---
+++ lib/widgets/bird.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Bird extends StatelessWidget {
+  final double y;
+  const Bird({super.key, required this.y});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment(0, y),
+      child: Image.asset("assets/bird.png"),
+    );
+  }
+}
